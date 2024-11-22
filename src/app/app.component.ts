e11cd0da02238a38861c89ad91d97c64e022bd86
Tang Weng Kitt <wengkitt10@outlook.com>
2024-11-22 23:39:16 +0800

    Add landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'wolio';
+  title = 'Wolio';
 }
